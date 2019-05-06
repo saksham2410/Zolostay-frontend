669d4d2733b97172e0dc7e1c58bce5d908ebe466
Saksham <gupta.10@iitj.ac.in>
2019-05-06 19:15:21 +0530

    Removed Production Build
    Fixed CORS issue.

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 const express = require('express')
-const {mongoose} = require('./config/database')
-const {routes} = require('./config/routes')
+const { mongoose } = require('./config/database')
+const { routes } = require('./config/routes')
 const port = 3005
-const app = express() 
+const app = express()
 
-app.use(express.json())
-app.use('/',routes)
+const cors = require('cors')
+app.use(cors())
 
-app.use(function(req, res, next){
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept" );
-    res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS");
+app.use(function(req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS");
     next();
-    
 });
 
-app.use(express.static('./public'))
+app.use(express.json())
+app.use('/', routes)
 
-app.listen(port,function(){
+// app.use(express.static('./public'))
+
+app.listen(port, function() {
     console.log(`Listening to ${port}........`)
 })
-
